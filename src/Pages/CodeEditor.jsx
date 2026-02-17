df4e796444208350991d3f0e70929b264ecad238
Harshal Belgamwar <153602780+Harshal-belgamwar@users.noreply.github.com>
2026-02-17 13:22:19 +0530

    CHANGE SOCKET URL

--- src/Pages/CodeEditor.jsx
+++ src/Pages/CodeEditor.jsx
@@ -10,7 +10,7 @@ import { io } from "socket.io-client";
 import Timer from "../components/Timer";
 import { toast } from "react-toastify";
 
-const BACKEND_URL = import.meta.env.VITE_API_URL;
+const BACKEND_URL = import.meta.env.VITE_API_URL_SOCKET;
 
 function encodeBase64(str) {
   const encoder = new TextEncoder();
